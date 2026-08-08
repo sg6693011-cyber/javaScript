43a6fc8a05280169168430b4445420c8357bc64e
Sahil_giri <sg6693011@gmail.com>
2026-08-08 04:28:07 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict";  //treat all js ode as newr version
+
+// alert(3+3) // we are using node js not browser
+
+console.log(3
+    +3) //code readibility should be high
+console.log("sahil")
+
+
+let name="sahil"
+let age=18
+let isloggedIn=false
+let state=null
+
+// integer or number =>2 to power 53
+// bigInt =>(use for very large value)
+// string =>" "
+// boolean=>true/false
+// null =>standlone value
+// undefined=>
+// symbol =>unique
+
+
+// object
+console.log(typeof "sahil"); //string
+console.log(typeof age); //number
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
