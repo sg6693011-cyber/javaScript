d91e94108a83b546ef3ecca4002b2d29a775f7c0
Sahil_giri <sg6693011@gmail.com>
2026-08-26 06:04:02 +0000

    object3

---
+++ 02_Basics/05_objects.js
@@ -0,0 +1,27 @@
+const course={
+    coursename:"js in hindi",
+    price:"999",
+    courseInstructor:"hitesh"
+}
+console.log(course.courseInstructor);
+// hitesh
+
+const{courseInstructor}=course
+console.log(courseInstructor);
+// hitesh
+
+const{courseInstructor:instructor}=course
+console.log(instructor);
+// hitesh
+
+
+//JSON
+{
+    "name":"sahil",
+    "coursename": "js in hindi",
+    "price":"free"
+}
+
+// https://api.github.com/users/hiteshchoudhary
+// randomuser.me
+// json formatter
