273ac98cdbe70bae91c8c452223b8a782fc8e968
Sahil_giri <sg6693011@gmail.com>
2026-09-02 20:55:30 +0000

    loop

---
+++ 05_iteration/two.js
@@ -0,0 +1,13 @@
+// while()
+
+let index=1;
+while(index<=10){
+    console.log(index*index);
+    index+=1;
+}
+
+let score=1;
+do{
+    console.log(`score : ${score}`);
+    score++;
+}while(score<=5)
